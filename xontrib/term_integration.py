5cc28935e2bc8440c87206f13c2ca32d44ebc387
eugenesvk <eugenesvk@users.noreply.github.com>
2023-02-15 23:37:10 +0700

    fix: exclude 'dumb'/'linux' terminals as they don't speak OSC

--- xontrib/term_integration.py
+++ xontrib/term_integration.py
@@ -9,9 +9,15 @@ env = XSH.env or {}
 
 # load during interactive sessions only
 if env.get("XONSH_INTERACTIVE", False):
-    if ("kitty" in os.getenv("TERMINFO", "")) or ("kitty" in os.getenv("TERM", "")):
-        import xontrib_term_integrations.kitty  # noqa
-    else:
-        # fallback
-        # if "iTerm" in os.getenv("TERM_PROGRAM", ""):
-        import xontrib_term_integrations.iterm2  # noqa
+    TERMINFO     = os.getenv("TERMINFO"    , "").lower()
+    TERM         = os.getenv("TERM"        , "").lower()
+    # avoid terminals that don't like OSC sequences
+    if  not TERM == "dumb" and\
+        not TERM == "linux":
+            if   ("kitty"   in TERMINFO    ) or\
+                 ("kitty"   in TERM        ):
+                import xontrib_term_integrations.kitty  # noqa
+            else:
+                # fallback
+                # if "iTerm" in os.getenv("TERM_PROGRAM", ""):
+                import xontrib_term_integrations.iterm2  # noqa
