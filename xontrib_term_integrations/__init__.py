6e955be8307e30ae88a1c83ec18d78ce6eaa58cb
Noortheen Raja <jnoortheen@gmail.com>
2023-02-16 11:32:11 +0530

    chore: replace flake8 with ruff

--- xontrib_term_integrations/__init__.py
+++ xontrib_term_integrations/__init__.py
@@ -1,1 +1,2 @@
-"""Each of the terminal integration should be put in its separate module and be imported from the xontrib"""
+"""Each of the terminal integration should be put in its separate module
+and be imported from the xontrib"""
